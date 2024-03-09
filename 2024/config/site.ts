a39a250bbd8f2ae1c105d7ab24af69817db68cbf
Aubron Wood <aubron@daemon.group>
2024-03-08 17:58:45 -0800

    Framework

---
+++ 2024/config/site.ts
@@ -0,0 +1,11 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: 'Aubron',
+  description:
+    'Aubron Wood is an engineering leader specializing in frontend technologies and design systems, working in the Seattle area.',
+  links: {
+    github: 'https://github.com/aubron',
+    email: 'mailto:aubron+website@daemon.group',
+  },
+};
